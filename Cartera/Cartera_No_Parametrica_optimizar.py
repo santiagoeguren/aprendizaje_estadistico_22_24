de806228f05d3995eadb50807643874d0c07fdc2
cmoyano <cristianmoyano.mza@gmail.com>
2022-09-30 12:14:51 -0300

    add weights

--- Cartera/Cartera_No_Parametrica_optimizar.py
+++ Cartera/Cartera_No_Parametrica_optimizar.py
@@ -42,23 +42,33 @@ num_ports=100000
 # Definir limites de los pesos
 weight_min = 1
 weight_max = 30
-weight_size = 7
+weight_size = len(ticker)
 
 
 #Ejecutar interacciones
 
-all_weights=np.zeros((num_ports,len(ticker)))
+all_weights=np.zeros((num_ports,weight_size))
 
 
 #Generar pesos
 
+def percent(value, decimals=2):
+     return round(Decimal(value.item()) / Decimal(100), decimals)
 
+
+def generate_weights(size, low=1, high=100):
+     """
+     Generar array de 'size' numeros en un intervalo [low, high).
+     # numpy.random.randint:
+          Return random integers from low (inclusive) to high (exclusive).
+          Return random integers from the “discrete uniform” distribution of the specified dtype in the “half-open” interval [low, high).
+          If high is None (the default), then results are from [0, low).
+     """
+     return map(percent, np.random.randint(low=low, high=high, size=size))
 
 for ind in range(num_ports):
-
-     weights=np.array(np.random.random(len(ticker)))
+     weights=np.array(generate_weights(size=weight_size, low=weight_min, high=weight_max))
      weights=weights/np.sum(weights)         
-   
      all_weights[ind:]=weights
 
 
